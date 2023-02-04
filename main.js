ec8bde57f9e14ede71c74bccc8da434ff552006a
Jesper <Jesperrudegran@gmail.com>
2023-02-04 19:45:32 +0100

    Cleanup

--- main.js
+++ main.js
@@ -5,5 +5,11 @@ window.onload = function () {
   menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
-  });
+  })
+
+  document.querySelectorAll(".mobile-nav a").forEach(n => n.
+    addEventListener("click", () => {
+        menu_btn.classList.remove("is-active");
+        mobile_menu.classList.remove("is-active");
+    }))
 }
